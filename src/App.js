2e8d6706a9be4cffeab1eb8ee07134f685caf235
Zioo Moo <571869136@qq.com>
2019-10-13 19:47:57 +0800

    ant标签栏结合路由设置

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { Button } from 'antd-mobile';
-function App() {
-  return (
-    <div className="App">
-      <Button>sdfs</Button>
-    </div>
-  );
+import React, { Component, Fragment } from 'react'
+import HKLayout from './components/HKLayout'
+
+import { HashRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import List from './pages/List'
+import News from './pages/News'
+import My from './pages/My'
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>
+          <Router>
+            <Route path='/' exact render={ () => <HKLayout><Home /></HKLayout>}></Route>
+            <Route path='/List' exact render={ () => <HKLayout><List /></HKLayout>}></Route>
+            <Route path='/News' exact render={ () => <HKLayout><News /></HKLayout>}></Route>
+            <Route path='/My' exact render={ () => <HKLayout><My /></HKLayout>}></Route>
+          </Router>
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default App;
