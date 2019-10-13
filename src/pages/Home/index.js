dada678828a5b2f5e49a16005666bb919a5c4223
Zioo Moo <571869136@qq.com>
2019-10-13 20:29:48 +0800

    axios设置及首页轮播图获取

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
+
+import { Carousel } from 'antd-mobile';
+
+import axios from '../../utils/request';
 
 export default class Home extends Component {
+    state = {
+        swiperData: [],
+        imgHeight: 176,
+    }
+    componentDidMount() {
+        axios.get('/home/swiper')
+        .then(res => {
+            // console.log(res)
+            this.setState({
+                swiperData: res.data.body
+            });
+        })
+    }
     render() {
         return (
-            <div>
-                Home
-            </div>
+            <Fragment>
+                {/* 轮播图 */}
+                {this.state.swiperData.length > 0 && <Carousel
+                autoplay
+                infinite
+                >
+                {this.state.swiperData.map(val => (
+                    <a
+                    key={val.id}
+                    href="http://www.alipay.com"
+                    style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                    >
+                    <img
+                        src={process.env.REACT_APP_API_URL + val.imgSrc}
+                        alt=""
+                        style={{ width: '100%', verticalAlign: 'top' }}
+                        onLoad={() => {
+                        window.dispatchEvent(new Event('resize'));
+                        this.setState({ imgHeight: 'auto' });
+                        }}
+                    />
+                    </a>
+                ))}
+                </Carousel>}
+                {/* 轮播图结束 */}
+            </Fragment>
         )
     }
 }
---
+++ src/utils/request.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+import { baseURL } from './url'
+
+let instance = axios.create({
+    baseURL
+  });
+
+export default instance
